255b52d414571865d049dd71ffc12a4224e552ae
Jordan Shaw <jrdnshw@gmail.com>
2016-12-09 23:04:07 -0500

    have json logging and propser data parsing for input data

--- serial_server.js
+++ serial_server.js
@@ -1,5 +1,16 @@
 var SerialPort = require('serialport');
+var _ = require('lodash');
+var jsonfile = require('jsonfile')
 var PubNub = require('pubnub');
+
+var globalSweepData = [];
+var sweepData = [];
+
+var saveFrequency = 30000;
+jsonfile.spaces = 4
+
+// var data_handler = _(parseData).bind(this);
+var data_handler = _.bind(parseData, this);
 
 var port = new SerialPort('/dev/cu.usbmodem1451', {
 	baudRate: 115200,
@@ -14,10 +25,95 @@ var port = new SerialPort('/dev/cu.usbmodem1451', {
         }
         console.log('message written');
     });
-    port.on('data', function(data) {
-        console.log(data);
-    });
+    // port.on('data', function(data) {
+    //     console.log(data);
+    // });
+    port.on('data', data_handler); 
 });
+
+function parseData(data){
+    console.log(data);
+
+    var tmp = _.split(data, '/');
+
+
+    var string_obj_handler = _.bind(stringToObject, this);
+    string_obj_handler
+    _.forEach(tmp, string_obj_handler); 
+
+    pushToGlobalArray();
+    logFullObject();
+}
+
+function stringToObject(str){
+    // console.log(str);
+    var tmp_array = _.split(str, ':');
+    
+    // console.log(_.indexOf(tmp_array, '\r'));
+
+    if(typeof tmp_array[0] != "undefined"
+        && typeof tmp_array[1] != "undefined"
+        && typeof tmp_array[2] != "undefined"
+        ){
+        var scan_tmp_object = {
+            'id': tmp_array[0].replace(/(?:\\[rn]|[\r\n]+)+/g, ""),
+            'ang': tmp_array[1].replace(/(?:\\[rn]|[\r\n]+)+/g, ""),
+            'dst': tmp_array[2].replace(/(?:\\[rn]|[\r\n]+)+/g, "")
+        };
+
+        // sweepData = sweepData.push(scan_tmp_object);
+        sweepData.push(scan_tmp_object);
+    }
+}
+
+function pushToGlobalArray(){
+    globalSweepData.push(sweepData);
+    clearSweepData();
+}
+
+function clearSweepData(){
+    sweepData = [];
+}
+
+function clearGlobalSweep(){
+    globalSweepData = [];
+}
+
+function logFullObject(){
+    console.log("sweepData: ", sweepData);
+    console.log("globalSweepData: ", globalSweepData);
+}
+
+function saveJSONFileHandler(err){
+    console.log(arguments);
+    console.error(err);
+    clearGlobalSweep();
+}
+
+var interval_handler = _.bind(intervalHandler, this);
+var interval = setInterval(interval_handler, saveFrequency);
+// clearInterval(interval);
+
+function intervalHandler(){
+    console.log("here???");
+
+
+    var tm_stamp = Date.now();
+
+    var file = './output/sweep_' + tm_stamp + '.json';
+
+    var save_json_handler = _.bind(saveJSONFileHandler, this);
+    jsonfile.writeFile(file, globalSweepData, {flag: 'w'}, save_json_handler);
+
+    //     function (err) {
+    //     console.error(err);
+    // });
+
+    // jsonfile.writeFile(file, obj, function (err) {
+    //     console.error(err);
+    // });
+
+}
 
 function publish() {
    
